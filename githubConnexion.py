4dcd1e08897a741a694f470cf2a586d4d3534b2e
Maxime MORA <maxime.mora@epitech.eu>
2021-02-02 11:46:31 +0100

    Push

---
+++ githubConnexion.py
@@ -0,0 +1,12 @@
+def githubConnect(driver, email, password, display) :
+    driver.get('https://github.com/login') #Get url with the driver
+
+    driver.find_element_by_id('login_field').send_keys(email) #Recuperation et remplissage du login
+    driver.find_element_by_id('password').send_keys(password) #Recuperation et remplissage du password
+    driver.find_element_by_name("commit").click() #Soumission du formulaire
+
+    if (display):
+        if (driver.title == "GitHub"): #Verification de connexion
+            print("Connecte sur le compte : " + email)
+        else:
+            print("Connexion non etablie")
---
+++ main.py
@@ -0,0 +1,14 @@
+from selenium import webdriver #Import the webdriver to launch Chrome driver here
+from selenium.webdriver.chrome.options import Options #Import Options from Selenium to configure our driver
+from githubConnexion import githubConnect
+
+options = Options() #Declare options
+options.headless = True #True=the driver won't be launch and False it will be launch
+options.add_argument("--window-size=1000,1080") #Size of driver's window
+options.add_argument("--incognito") #If you add this argument, the driver will launch a private session
+DRIVER_PATH = "your_driver_path" #Replace by the path of your driver's executable
+driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH) #Create and init with options your driver
+
+githubConnect(driver, "your_github_email", "your_github_password", False) #Connect to github
+
+driver.quit() #Close the driver at the end of the program
